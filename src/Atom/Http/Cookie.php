518153ef30a826e3ea5630b0ce28bc4eaa698ccd
Chase Hutchins <syntaqx@gmail.com>
2012-06-10 20:45:21 -0600

    added header and cookie structs, and status texts

---
+++ src/Atom/Http/Cookie.php
@@ -0,0 +1,13 @@
+<?php namespace Atom\Http;
+
+/**
+ * Cookie class
+ *
+ * The Cookie class represents an HTTP Cookie
+ *
+ * @package    Atom
+ * @subpackage Http
+ */
+class Cookie
+{
+}
---
+++ src/Atom/Http/Header.php
@@ -0,0 +1,13 @@
+<?php namespace Atom\Http;
+
+/**
+ * Header class
+ *
+ * The Header class represents an HTTP header
+ *
+ * @package    Atom
+ * @subpackage Http
+ */
+class Header
+{
+}
