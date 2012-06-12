176f153e846557da98ad6e7f5572cad97cf58f5f
fission1110 <rysnxedge1110@gmail.com>
2012-06-11 19:49:34 -0600

    added the singleton interface into the response namespace, as well as a README.md

--- src/Atom/Http/Response.php
+++ src/Atom/Http/Response.php
@@ -8,4 +8,45 @@ namespace Atom\Http;
  **/
 class Response
 {
+		/**
+		 * Static holder for Cookie object
+		 *
+		 * @var string
+		 **/
+		private static $Cookie;
+
+		/**
+		 * Static holder for Header object
+		 *
+		 * @var string
+		 **/
+		private static $Header;
+
+		/**
+		 * Singleton Cookie instantiating method
+		 *
+		 * @return Object Cookie
+		 **/
+		private static function Cookie()
+		{
+				if(!self::$Cookie)
+				{
+						self::$Cookie = new Response\Cookie;
+				}
+				return self::$Cookie;
+		}
+
+		/**
+		 * Singleton Header instantiating method
+		 *
+		 * @return Object Header
+		 **/
+		private static function Header()
+		{
+				if(!self::$Header)
+				{
+						self::$Header = new Response\Header;
+				}
+				return self::$Header;
+		}
 } // END class Response
